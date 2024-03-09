132b1c24d8855c1bd124edb04b3872c68eec322f
Oleksandra Tkachuk <144816647+tkachuk-ai@users.noreply.github.com>
2024-03-09 17:50:17 -0500

    Update Brute_Force_Solution.java

--- 1.Two-Sum/Brute_Force_Solution.java
+++ 1.Two-Sum/Brute_Force_Solution.java
@@ -1,4 +1,4 @@
-public class Brute_Force_Solution {
+class Brute_Force_Solution {
   public int[] twoSum(int[] nums, int target) {
     for (int i = 0; i != nums.length; ++i) {
       for (int j = i + 1; j != nums.length; ++j) {
