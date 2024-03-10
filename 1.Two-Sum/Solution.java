f107daedaba69899d176f08f17cc213fec281a08
Oleksandra Tkachuk <144816647+tkachuk-ai@users.noreply.github.com>
2024-03-09 22:05:45 -0500

    Update and rename Brute_Force_Solution.java to Solution.java

--- 1.Two-Sum/Solution.java
+++ 1.Two-Sum/Solution.java
@@ -1,10 +1,11 @@
+class Solution {
 /** 
+Brute Force 
 Analysis:
 Time Complexity: O(n^2)
 Space Complexity: O(1)
   */
-
-class Brute_Force_Solution {
+ 
   public int[] twoSum(int[] nums, int target) {
     for (int i = 0; i != nums.length; ++i) {
       for (int j = i + 1; j != nums.length; ++j) {
